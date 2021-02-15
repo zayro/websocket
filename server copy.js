0184a28180ad44e16aa9832ecad78bd832ad524b
marlon zayro <zayro8905@gmail.com>
2021-02-15 09:22:23 -0500

    socket version 2

--- server copy.js
+++ server copy.js
@@ -6,7 +6,9 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
   }
 });
 
--- server.js
+++ server.js
@@ -10,9 +10,18 @@ app.get('/', (req, res) => {
 const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
+    console.log('connection');
     userSocket.on("send_message", (data) => {
+        console.log(data);
         userSocket.broadcast.emit("receive_message", data)
+    });
+
+    userSocket.on("msg", (data) => {
+        console.log(data);        
     })
+
+
+
 })
 
 http.listen(process.env.PORT || 3000)
